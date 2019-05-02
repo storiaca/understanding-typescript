5a2e607df8557c88fbf04afbee77f1aed2f0562f
storiaca <storiaca@gmail.com>
2019-05-02 13:19:09 +0200

    Numbers and boolens

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 // string
 let myName = "Max";
 // myName = 28; app.ts:3:1 - error TS2322: Type '28' is not assignable to type 'string'.
+
+// number
+let myAge = 27;
+// myAge = 'Max'; error
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1; error
