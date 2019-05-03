c9c8cf21c5a6fd14d0fc7e8565516a6b3239b8e9
Aleksandar Ristic <storiaca@gmail.com>
2019-05-03 13:20:25 +0200

    Array and types

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 "use strict";
-// string
+/**
+ * String type
+ */
 var myName = "Max";
 // myName = 28; app.ts:3:1 - error TS2322: Type '28' is not assignable to type 'string'.
-// number
+/**
+ * Number type
+ */
 var myAge = 27;
 // myAge = 'Max'; error
-// boolean
+/**
+ * Boolean type
+ */
 var hasHobbies = false;
 // hasHobbies = 1; error
 // assign types
 var myRealAge;
 myRealAge = 27;
-myRealAge = '27';
+// myRealAge = '27';
+/**
+ * Array type
+ */
+var hobbies = ["Cooking", "Sports"];
+console.log(typeof hobbies);
--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-// string
+/**
+ * String type
+ */
 let myName: string = "Max";
 // myName = 28; app.ts:3:1 - error TS2322: Type '28' is not assignable to type 'string'.
 
-// number
+/**
+ * Number type
+ */
 let myAge: number = 27;
 // myAge = 'Max'; error
 
-// boolean
+/**
+ * Boolean type
+ */
 let hasHobbies: boolean = false;
 // hasHobbies = 1; error
 
@@ -14,3 +20,9 @@ let hasHobbies: boolean = false;
 let myRealAge: number;
 myRealAge = 27;
 // myRealAge = '27';
+
+/**
+ * Array type
+ */
+let hobbies: any[] = ["Cooking", "Sports"];
+hobbies = [100];
