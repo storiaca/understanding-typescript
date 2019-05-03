22763bc85cf2adafa533a99384f25aa37822528f
Aleksandar Ristic <storiaca@gmail.com>
2019-05-03 12:56:51 +0200

    Assigning types explicitly

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 "use strict";
 // string
 var myName = "Max";
-myName = 28;
+// myName = 28; app.ts:3:1 - error TS2322: Type '28' is not assignable to type 'string'.
+// number
+var myAge = 27;
+// myAge = 'Max'; error
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1; error
+// assign types
+var myRealAge;
+myRealAge = 27;
+myRealAge = '27';
--- app.ts
+++ app.ts
@@ -1,11 +1,16 @@
 // string
-let myName = "Max";
+let myName: string = "Max";
 // myName = 28; app.ts:3:1 - error TS2322: Type '28' is not assignable to type 'string'.
 
 // number
-let myAge = 27;
+let myAge: number = 27;
 // myAge = 'Max'; error
 
 // boolean
-let hasHobbies = false;
+let hasHobbies: boolean = false;
 // hasHobbies = 1; error
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
